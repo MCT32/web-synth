c6f6ec438d6787ec7c420255ae7c1e4b5cb43964
MCT32 <32090502+MCT32@users.noreply.github.com>
2024-03-27 14:20:32 +1100

    MIDI INPUT BABY

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,58 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import { onMount } from 'svelte';
+    import * as Tone from 'tone';
+
+
+    let synth: Tone.Synth;
+
+    const keys: Array<string> = [
+        "C",
+        "C#",
+        "D",
+        "D#",
+        "E",
+        "F",
+        "F#",
+        "G",
+        "G#",
+        "A",
+        "A#",
+        "B",
+    ]
+
+    function onMIDIMessage(event: Event) {
+        if (((event as MIDIMessageEvent).data[0] & 0b11110000) == 0b10010000) {
+            const octave = Math.floor((event as MIDIMessageEvent).data[1] as number / 12) - 2;
+            const key = keys[(event as MIDIMessageEvent).data[1] as number % 12];
+
+            synth.triggerAttack(key + octave);
+        }
+
+        if (((event as MIDIMessageEvent).data[0] & 0b11110000) == 0b10000000) {
+            synth.triggerRelease();
+        }
+    }
+
+    onMount(async () => {
+        let midi: MIDIAccess;
+
+        await navigator.requestMIDIAccess().then((midiAccess) => {
+            midi = midiAccess;
+
+            midi.inputs.forEach((entry) => {
+                entry.onmidimessage = onMIDIMessage;
+                console.log(entry.name);
+            })
+        });
+    });
+
+    function start() {
+        const filter = new Tone.Filter(500, "lowpass").toDestination();
+        synth = new Tone.Synth().connect(filter);
+        
+        synth.oscillator.type = 'square';
+    }
+</script>
+
+
+<button on:click={start}>play</button>
